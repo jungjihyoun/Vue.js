07e2d36278dcf8374b3c20824d6e4e594ddb8a9d
jungjihyoun <jihyoun0602@gmail.com>
2021-01-27 14:16:48 +0900

    CLI 마무리

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- HTML -->
+  <div>Hello</div>
+</template>
+
+
+
+<script>
+export default {
+    // Javascript - 인스턴스 옵션
+
+}
+</script>
+
+
+
+<style>
+/* CSS */
+
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <h1>{{propsdata}}</h1>
+      <button v-on:click="sendEvent">send</button>
+  </header>
+
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods:{
+        sendEvent: function(){
+            this.$emit('renew');
+            //위쪽 컴포넌트로 전달 
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
